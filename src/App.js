63d668ba0bab3128d05425546976eb9a83c8a1fe
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2023-11-01 14:18:15 +0530

    adding some expense

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
-
+      <h2>Let get Started</h2>
+      <ExpenseItem/>
     </div>
   );
 }
