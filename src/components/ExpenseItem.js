be1cb3940487a8454a5902407ebf34331e14bbc2
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2023-11-01 18:45:17 +0530

    making component smaller

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import '../css/ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
+import ExpenseDetails from './ExpenseDetails'
 
 function ExpenseItem(props) {
-  console.log(props,'f');
   return (
     <div className="expense-item">
-      {/* <h2>hh</h2> */}
-        <div> {props.date.toDateString()} </div>
-        <div className="expense-item_description">
-          <h2> {props.title} </h2>
+    <ExpenseDate date={props.date} />
+    <ExpenseDetails amount={props.amount} title={props.title} />
+      {/* <div className="expense-item_description">
+        <h2> {props.title} </h2>
         <div className="expense-item__price">{props.amount}</div>
-        </div>
+      </div> */}
     </div>
   )
 }
